547318eb73e12e5281b71ad67dfba075462856f1
bking1989 <bradley_king@me.com>
2019-01-28 18:22:04 -0700

    Added dummy data to the database, trying to get the survey data to parse into values

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,32 @@
-var friendArray = [];
+var friendArray = [
+    {
+        memberName: "Jon Snow",
+        memberAge: 22,
+        memberLocation: "Winterfell, Westeros",
+        memberPicture: "https://gameofthrones.fandom.com/wiki/File:Profile-JonSnow-707.png",
+        memberQuiz: [4,4,5,3,3]
+    },
+    {
+        memberName: "Cersei Lannister",
+        memberAge: 22,
+        memberLocation: "King's Landing, Westeros",
+        memberPicture: "https://gameofthrones.fandom.com/wiki/File:Profile-CerseiLannister.png",
+        memberQuiz: [1,5,5,2,3]
+    },
+    {
+        memberName: "Sandor Clegane",
+        memberAge: 35,
+        memberLocation: "Winterfell, Westeros",
+        memberPicture: "https://gameofthrones.fandom.com/wiki/File:Sandor_Clegane_Season_7_Promo_Image.PNG",
+        memberQuiz: [3,4,1,2,5]
+    },
+    {
+        memberName: "Daenerys Targaryen",
+        memberAge: 22,
+        memberLocation: "Dragonstone, Westeros",
+        memberPicture: "https://gameofthrones.fandom.com/wiki/File:Daenerys_Dragonpit.jpg",
+        memberQuiz: [3,5,3,4,2]
+    }
+];
 
 module.exports = friendArray;
